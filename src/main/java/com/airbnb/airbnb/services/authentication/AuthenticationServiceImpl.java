4ad26d0ee2c2e22b9a6954da8dd7f5b41c62d3ea
mealibek <mealibek@hotmail.com>
2023-12-16 22:41:24 +0600

    Auth Role User Added.

--- src/main/java/com/airbnb/airbnb/services/authentication/AuthenticationServiceImpl.java
+++ src/main/java/com/airbnb/airbnb/services/authentication/AuthenticationServiceImpl.java
@@ -7,6 +7,10 @@ import com.airbnb.airbnb.enums.Role;
 import com.airbnb.airbnb.models.User;
 import com.airbnb.airbnb.repositories.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -17,6 +21,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
 
     private final JWTAuthenticationService jwtAuthenticationService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public AuthenticationResponse signUp(AuthenticationRequest authenticationRequest) {
@@ -25,6 +32,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .password(authenticationRequest.getPassword())
                 .role(Role.USER)
                 .build();
+        String encodedPass = passwordEncoder.encode(authenticationRequest.getPassword());
+        user.setPassword(encodedPass);
         userRepository.save(user);
         return AuthenticationResponse
                 .builder()
@@ -35,11 +44,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationResponse signIn(String email, String password) {
-        User user = userRepository.findUserByEmail(email).orElseThrow(() -> new RuntimeException("Not found"));
-        return  AuthenticationResponse
-                .builder()
+        var user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new BadCredentialsException(
+                        String.format("Email %s is not registered!", email)
+                ));
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        password
+                )
+        );
+        var jwtToken = jwtAuthenticationService.generateToken(user);
+        return AuthenticationResponse.builder()
                 .email(user.getEmail())
-                .token(jwtAuthenticationService.generateToken(user))
+                .token(jwtToken)
                 .build();
     }
 }
